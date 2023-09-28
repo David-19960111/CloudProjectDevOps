887151a320c8eb9bf3eb3a4d986d63a7f684c180
David-19960111 <david11011996@gmail.com>
2023-09-27 23:00:37 -0300

    Update

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+PROJECT_ID = "devopsproject-400013"
+REGION = "us-central1"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "PROJECT_ID" {
+  type = string
+  default = "devopsproject-400013"
+}
+
+variable "REGION" {
+  type = string
+  default = "us-central1"
+}
